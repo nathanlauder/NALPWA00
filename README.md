de78f42e1af379389aadaf67adfdcd75aa60082f
Nathan Lauder <56481793+nlauder42@users.noreply.github.com>
2021-03-12 11:28:36 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PWA00NAL
+Repo for 581 AppDev as we create a PWA
---
+++ sw.js
@@ -0,0 +1,33 @@
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
+const PRECACHE_URLS = [
+  'index.html',
+  './', //index.html alias
+  'css/style.css',
+  'js/main.js',
+  'sw.js'
+  ];
+
+// install handler precaches our resources as directed
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(PRECACHE).then(function(cache) {
+      return cache.addAll(PRECACHE_URLS);
+    })
+    );
+});
+
+// activate handler
+self.addEventListener('activate', event => {
+  console.log('Service work up...');
+});
+
+// the fetch handler
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+    });
+});
