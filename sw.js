c3b64b2fb3aa8392c6e8a41116c0001550a9a02d
nathanlauder <56481793+nathanlauder@users.noreply.github.com>
2021-03-16 13:42:41 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,41 @@
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
+// A list of local resources we always want to be cached
 const PRECACHE_URLS = [
   'index.html',
-  './', //index.html alias
+  './', //alias for index.html
   'css/style.css',
   'js/main.js',
   'images/hello-icon-128.png',
   'images/hello-icon-144.png',
   'images/hello-icon-152.png',
   'images/hello-icon-192.png',
+  'images/hello-icon-196maskable.png',
   'images/hello-icon-256.png',
   'images/hello-icon-512.png',
-  'images/hello-icon-196maskable.png',
   'sw.js'
   ];
 
-// install handler precaches our resources as directed
-self.addEventListener('install', function(event) {
+// The install handler takes care of precaching our resources as directed
+self.addEventListener('install',function(event){
   event.waitUntil(
-    caches.open(PRECACHE).then(function(cache) {
+    caches.open(PRECACHE).then(function(cache){
       return cache.addAll(PRECACHE_URLS);
     })
     );
 });
 
-// activate handler
-self.addEventListener('activate', event => {
-  console.log('Service work up...');
+// The activate handler 
+self.addEventListener('activate',event => {
+  console.log('Service worker activating . . . ');
 });
 
-// the fetch handler
+// The fetch handler
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
     })
-    });
+    );
 });
